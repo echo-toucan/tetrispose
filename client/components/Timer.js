bd9b13a5497dd06ce85003105583546ac0f3ebce
Sunny Chang Mei <mkaleader@gmail.com>
2019-06-27 12:34:39 -0400

    create display component

---
+++ client/components/Timer.js
@@ -0,0 +1,10 @@
+// import {Menu, Container} from 'semantic-ui-react'
+import React, {Component} from 'react'
+
+class CurrentShape extends Component {
+  render() {
+    return <h1>Display Timer Here</h1>
+  }
+}
+
+export default CurrentShape
