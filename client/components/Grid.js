305dcabe000d2b4963f9009d569b843265f49a1e
Amneet Sandhu <amneet954@gmail.com>
2019-06-28 15:56:16 -0400

    work in progress

--- client/components/Grid.js
+++ client/components/Grid.js
@@ -26,7 +26,28 @@ let grid = [
 ]
 
 export default class Grid extends Component {
+  constructor() {
+    super()
+  }
+  //ADVANCES TIME
+  // ONE THAT MOVES
+  hasSpaceAbove(x, y) {
+    if (grid[y - 1] && grid[y - 1][x] === 0) return true
+    else return false
+  }
+  hasSpaceBelow(x, y) {
+    if (grid[y + 1] && grid[y + 1][x] === 0) return true
+    else return false
+  }
+  updateBoard(oldGrid = grid) {
+    let newGrid = oldGrid.map((row, rowIdx) => {
+      return row.map((cell, cellIdx) => {
+        if (this.hasSpaceBelow(cellIdx, rowIdx) && cell === 1) return cell
+      })
+    })
+  }
   render() {
+    console.log(this.hasSpaceBelow(8, 19))
     return (
       <div>
         <Table>
