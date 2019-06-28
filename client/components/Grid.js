7ed209a9688dffdb628b758e9eb876266756f3a5
tjhelsel <terence.helsel@gmail.com>
2019-06-28 16:28:57 -0400

    feat: numbers automatically fall and stack using hardcoded input

--- client/components/Grid.js
+++ client/components/Grid.js
@@ -2,57 +2,101 @@ import React, {Component} from 'react'
 import GridRow from './GridRow'
 import {Table} from 'semantic-ui-react'
 
-let grid = [
-  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
+// let grid = [
+//   [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+// ]
 
 export default class Grid extends Component {
   constructor() {
     super()
+    this.state = {
+      grid: [
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ]
+    }
+    this.updateBoard = this.updateBoard.bind(this)
   }
   //ADVANCES TIME
   // ONE THAT MOVES
+
+  drop() {
+    setInterval(this.updateBoard, 500)
+  }
   hasSpaceAbove(x, y) {
-    if (grid[y - 1] && grid[y - 1][x] === 0) return true
+    const grid = this.state.grid
+    if (!grid[y - 1] || grid[y - 1][x] === 0) return true
     else return false
   }
   hasSpaceBelow(x, y) {
+    const grid = this.state.grid
     if (grid[y + 1] && grid[y + 1][x] === 0) return true
     else return false
   }
-  updateBoard(oldGrid = grid) {
+  updateBoard() {
+    const oldGrid = this.state.grid
+
     let newGrid = oldGrid.map((row, rowIdx) => {
       return row.map((cell, cellIdx) => {
-        if (this.hasSpaceBelow(cellIdx, rowIdx) && cell === 1) return cell
+        if (cell === 0 && !this.hasSpaceAbove(cellIdx, rowIdx)) {
+          return 1
+        } else if (
+          cell === 1 &&
+          this.hasSpaceAbove(cellIdx, rowIdx) &&
+          this.hasSpaceBelow(cellIdx, rowIdx)
+        ) {
+          return 0
+        } else return cell
       })
     })
+    this.setState({grid: newGrid})
   }
   render() {
-    console.log(this.hasSpaceBelow(8, 19))
     return (
       <div>
+        <button type="button" onClick={() => this.drop()}>
+          Drop!
+        </button>
         <Table>
           <Table.Body>
-            {grid.map((row, rowIdx) => {
+            {this.state.grid.map((row, rowIdx) => {
               return (
                 <Table.Row key={rowIdx}>
                   {row.map((cell, cellIdx) => {
