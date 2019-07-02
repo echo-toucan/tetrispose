bc26f654c2068f24f94da94b1025e56a0fdd4f99
tjhelsel <terence.helsel@gmail.com>
2019-07-02 12:02:47 -0400

    style: user pose appears next to target pose

--- client/GameHome.js
+++ client/GameHome.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react'
-import {Grid, GridColumn, Segment} from 'semantic-ui-react'
+import {Grid, GridRow, GridColumn, Segment} from 'semantic-ui-react'
 import {
   Camera,
-  Timer,
   PreviewShape,
   GameBoard,
   CurrentShape,
@@ -13,29 +12,36 @@ class GameHome extends Component {
   render() {
     return (
       <Grid container columns={2} padded>
-        <GridColumn floated="left" width={10}>
-          {/* <Segment>
-            <Timer />
-          </Segment> */}
-          <Segment>
-            <Camera />
-          </Segment>
-          <Segment color="red" inverted>
-            <UserShape />
-          </Segment>
-          <Segment color="orange" inverted>
-            <CurrentShape />
-          </Segment>
-          <Segment color="blue" inverted>
-            <PreviewShape />
-          </Segment>
-        </GridColumn>
+        <Grid.Column floated="left" width={10}>
+          <Grid.Row>
+            <Segment>
+              <Camera />
+            </Segment>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid padded container>
+              <Grid.Column width={8}>
+                <Segment color="orange" inverted>
+                  <CurrentShape />
+                </Segment>
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <Segment color="red" inverted>
+                  <UserShape />
+                </Segment>
+              </Grid.Column>
+              <Segment color="blue" inverted width={16}>
+                <PreviewShape />
+              </Segment>
+            </Grid>
+          </Grid.Row>
+        </Grid.Column>
 
-        <GridColumn floated="right" width={6}>
+        <Grid.Column floated="right" width={6}>
           <Segment color="green" inverted>
             <GameBoard />
           </Segment>
-        </GridColumn>
+        </Grid.Column>
       </Grid>
     )
   }
