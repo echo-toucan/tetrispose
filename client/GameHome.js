6585491ada89242862e760f4a0d541831ea6a7e8
Sunny Chang Mei <mkaleader@gmail.com>
2019-07-02 17:55:11 -0400

    clear semantic

--- client/GameHome.js
+++ client/GameHome.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Grid, GridRow, GridColumn, Segment} from 'semantic-ui-react'
+import {Grid, Segment} from 'semantic-ui-react'
 import {connect} from 'react-redux'
 import {
   Camera,
