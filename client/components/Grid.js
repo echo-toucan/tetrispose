81948a35bbb8ef6f266b24ca9238dc248091e2dc
Hari.Doshi <hdoshi2@gmail.com>
2019-07-09 11:38:27 -0400

    fix: changed game interval back to .5 sec

--- client/components/Grid.js
+++ client/components/Grid.js
@@ -64,7 +64,7 @@ class Grid extends Component {
 
   //sets the tetris board speed
   drop() {
-    setInterval(this.updateBoard, 100)
+    setInterval(this.updateBoard, 500)
   }
 
   //it updates the board when an active shape moves down or lands
