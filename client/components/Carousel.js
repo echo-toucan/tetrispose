369cca7cd65a3167e94e6fd9bd620762231689f8
Sunny Chang Mei <mkaleader@gmail.com>
2019-07-10 14:38:57 -0400

    update image

--- client/components/Carousel.js
+++ client/components/Carousel.js
@@ -11,24 +11,12 @@ const ControlledCarousel = () => {
     },
     {
       render: () => {
-        return (
-          <Image
-            centered
-            width="800px"
-            src="https://images.unsplash.com/photo-1485742217969-f1e4cd444fe8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2557&q=80"
-          />
-        )
+        return <Image centered width="1000px" src="/assets/showgame.gif" />
       }
     },
     {
       render: () => {
-        return (
-          <Image
-            centered
-            width="800px"
-            src="https://images.unsplash.com/photo-1485742217969-f1e4cd444fe8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2557&q=80"
-          />
-        )
+        return <Image centered width="1000px" src="/assets/showgame.gif" />
       }
     }
   ]
