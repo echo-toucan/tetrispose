542621a3f64d9ac1eafb51bcb5053c317a11aa25
Hari.Doshi <hdoshi2@gmail.com>
2019-07-15 18:04:03 -0400

    refactor: moved GameHome component

--- client/GameHome.js
+++ client/GameHome.js
@@ -1,80 +1,80 @@
-import React, {Component} from 'react'
-import {Grid, Segment} from 'semantic-ui-react'
-import {connect} from 'react-redux'
-import {
-  Camera,
-  PreviewShape,
-  GameBoard,
-  CurrentShape,
-  UserShape,
-  SuccessMessage,
-  GamePanel,
-  Score
-} from './components'
+// import React, {Component} from 'react'
+// import {Grid, Segment} from 'semantic-ui-react'
+// import {connect} from 'react-redux'
+// import {
+//   Camera,
+//   PreviewShape,
+//   GameBoard,
+//   CurrentShape,
+//   UserShape,
+//   SuccessMessage,
+//   GamePanel,
+//   Score
+// } from './components'
 
-class GameHome extends Component {
-  render() {
-    const {achieved, phase} = this.props
+// class GameHome extends Component {
+//   render() {
+//     const {achieved, phase} = this.props
 
-    return (
-      <Grid padded columns={3}>
-        <Grid.Column width={8}>
-          <Grid.Row>
-            <Segment>
-              <Camera />
-            </Segment>
-          </Grid.Row>
+//     return (
+//       <Grid padded columns={3}>
+//         <Grid.Column width={8}>
+//           <Grid.Row>
+//             <Segment>
+//               <Camera />
+//             </Segment>
+//           </Grid.Row>
 
-          <Grid.Row>
-            {achieved || (!achieved && phase === 2) ? (
-              <Segment color="orange" inverted secondary textAlign="center">
-                <SuccessMessage />
-              </Segment>
-            ) : (
-              <Grid padded columns={2}>
-                <Grid.Column width={12}>
-                  <Segment color="yellow" inverted secondary raised>
-                    <CurrentShape />
-                  </Segment>
-                </Grid.Column>
+//           <Grid.Row>
+//             {achieved || (!achieved && phase === 2) ? (
+//               <Segment color="orange" inverted secondary textAlign="center">
+//                 <SuccessMessage />
+//               </Segment>
+//             ) : (
+//               <Grid padded columns={2}>
+//                 <Grid.Column width={12}>
+//                   <Segment color="yellow" inverted secondary raised>
+//                     <CurrentShape />
+//                   </Segment>
+//                 </Grid.Column>
 
-                <Grid.Column width={4}>
-                  <Segment color="red" inverted secondary>
-                    <UserShape />
-                  </Segment>
-                </Grid.Column>
-              </Grid>
-            )}
-          </Grid.Row>
-        </Grid.Column>
+//                 <Grid.Column width={4}>
+//                   <Segment color="red" inverted secondary>
+//                     <UserShape />
+//                   </Segment>
+//                 </Grid.Column>
+//               </Grid>
+//             )}
+//           </Grid.Row>
+//         </Grid.Column>
 
-        <Grid.Column width={2} divided="true">
-          <Segment>
-            <GamePanel centered />
-          </Segment>
+//         <Grid.Column width={2} divided="true">
+//           <Segment>
+//             <GamePanel centered />
+//           </Segment>
 
-          <Segment>
-            <Score />
-          </Segment>
+//           <Segment>
+//             <Score />
+//           </Segment>
 
-          <Segment>
-            <PreviewShape />
-          </Segment>
-        </Grid.Column>
+//           <Segment>
+//             <PreviewShape />
+//           </Segment>
+//         </Grid.Column>
 
-        <Grid.Column width={6}>
-          <Segment color="green" inverted secondary raised>
-            <GameBoard />
-          </Segment>
-        </Grid.Column>
-      </Grid>
-    )
-  }
-}
+//         <Grid.Column width={6}>
+//           <Segment color="green" inverted secondary raised>
+//             <GameBoard />
+//           </Segment>
+//         </Grid.Column>
+//       </Grid>
+//     )
+//   }
+// }
 
-const mapStateToProps = state => ({
-  achieved: state.currentShape.achieved,
-  phase: state.phase
-})
+// const mapStateToProps = state => ({
+//   achieved: state.currentShape.achieved,
+//   phase: state.phase
+// })
 
-export default connect(mapStateToProps)(GameHome)
+// export default connect(mapStateToProps)(GameHome)
